720784322bc83b7ca95005e72d235e8cc29126fc
Anna Kiniova <anna.kiniova@gmail.com>
2021-09-09 00:04:07 +0300

    task 4. Adjust routes

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
     "https://7yzrb5wzg6.execute-api.eu-west-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: "https://7yzrb5wzg6.execute-api.eu-west-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 
